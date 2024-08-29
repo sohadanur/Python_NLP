8823977098bb9dd94ca6cf203b4596bc14a1facb
Sohada Hasan <sohadanur01@gmail.com>
2024-08-29 13:00:51 +0600

    Update audio_to_text.py
    https://colab.research.google.com/drive/1RV8fa82IAJKSjC0MKGt1W_ElFFVrJmzr#scrollTo=hE12U906khf9

--- audio_to_text.py
+++ audio_to_text.py
@@ -6,6 +6,9 @@ r = sr.Recognizer()
 #  listening  the  аudiо  file  аnd  stоre  in  аudiо_text  vаriаble
 with sr.AudioFile(r"C:\Users\sohad\OneDrive\Desktop\Python\BacBon\harvard.wav") as source:
     audio_text = r.listen(source)
+#This line is to get the source file from google collab, if you're working on collab.
+#with sr.AudioFile(r"/content/harvard (1).wav") as source:
+        #audio_text = r.listen(source)
 # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
     try:
         # using google speech recognition
